5f1799c6cf1070c5a3dc17c26d20002efb8b1cf8
KawuMusa <kawumusai@gmail.com>
2022-03-10 20:26:56 -0800

    pi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -9,7 +8,4 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-     printf("with proper grammar, but the outcome is a piece of art,\n");
-     return (0);
-}
+{printf("with proper grammar, but the outcome is a piece of art,\n");return (0);}
