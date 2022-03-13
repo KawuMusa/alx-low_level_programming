b047bda09bae449ad3f7b3a741d6f85259e48696
KawuMusa <kawumusai@gmail.com>
2022-03-12 18:08:06 -0800

    ew

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -29,10 +29,11 @@ int main(void)
 		
 				if (i < 89)
 				{
-																																			putchar(44);
-																																			putchar(32);
+					putchar(44);
+					putchar(32);
 																																		}
 		}
+
 		i++;
 	}
 	putchar('\n');
