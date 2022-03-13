29bbf9fb9c21751c4225656ac80dd8e20f606ede
KawuMusa <kawumusai@gmail.com>
2022-03-12 16:58:20 -0800

    ree

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/** Main - Main function 
+ * Description - Prints alpha in lowercase, uppercase, then new line.
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return(0);
+}
