df16d039bdcbbfe0bbd220c523f5a1230457c367
KawuMusa <kawumusai@gmail.com>
2022-03-12 17:05:58 -0800

    we

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char c = 'a';
 
-	while ( c <= 'z')
+	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
@@ -16,7 +16,7 @@ int main(void)
 
 	c = 'A';
 
-	while ( c <= 'Z')
+	while (c <= 'Z')
 	{
 		putchar(c);
 		c++;
@@ -24,5 +24,5 @@ int main(void)
 
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
